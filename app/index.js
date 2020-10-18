bacf3c52bf6e690bb65c2c4b7a1a0819a981467e
Antonio Scardovi <antonio.scardovi@gmail.com>
2020-10-18 13:09:50 +0200

    accordion auto-close

--- app/index.js
+++ app/index.js
@@ -1,16 +1,25 @@
+let acc = document.getElementsByClassName("accordion");
 let question = document.getElementsByClassName("question");
+let answer = document.getElementsByClassName("answer");
+let arrow = document.getElementsByClassName("arrow");
 
-for (let i = 0; i < question.length; i++) {
-  question[i].addEventListener("click", function () {
-    // console.log(this.nextElementSibling);
-    let answer = this.nextElementSibling;
-    let arrow = this.firstElementChild;
-    if (answer.style.maxHeight) {
-      answer.style.maxHeight = null;
-      arrow.style.transform = null;
-    } else {
-      answer.style.maxHeight = answer.scrollHeight + "px";
-      arrow.style.transform = "rotateZ(180deg)";
+for (var i = 0; i < question.length; i++) {
+  question[i].onclick = function () {
+    var setClasses = !this.classList.contains("active");
+    setClass(question, "active", "remove");
+    setClass(answer, "show", "remove");
+    setClass(arrow, "arrow-up", "remove");
+
+    if (setClasses) {
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("show");
+      this.firstElementChild.classList.toggle("arrow-up");
     }
-  });
+  };
+}
+
+function setClass(els, className, fnName) {
+  for (var i = 0; i < els.length; i++) {
+    els[i].classList[fnName](className);
+  }
 }
